fd899ddf532949f8f44e1bc8550d769f6757ffea
Kebab95 <kebalazs95@gmail.com>
2017-02-13 09:05:23 +0100

    Main Layout #2 About és Game layout létrehozva. Gombokra megnyitódnak

--- app/src/main/java/com/example/kebab/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/kebab/tictactoe/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kebab.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Button about = (Button)findViewById(R.id.about);
+        Button game = (Button)findViewById(R.id.game);
+        Button highscore = (Button)findViewById(R.id.highscore);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(i);
+            }
+        });
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
